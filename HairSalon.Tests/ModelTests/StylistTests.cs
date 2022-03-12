d4aaa6e8a46018423f30b17484368dc6ab87c2bb
Patrick Dolan <dolanp1992@gmail.com>
2022-03-12 15:13:13 -0800

    change testing files to match hairsalon project

---
+++ HairSalon.Tests/ModelTests/StylistTests.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HairSalon.Models;
+
+namespace HairSalon.Tests
+{
+  [TestClass]
+  public class StylistTests
+  {
+    // Test methods go here
+  }
+}
